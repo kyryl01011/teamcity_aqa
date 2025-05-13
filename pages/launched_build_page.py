7c60704b5c9db11516fcc1371a77db2c876e5f92
Apacha1 <mrpepperjam@gmail.com>
2025-05-13 20:47:55 +0200

    change locators

--- pages/launched_build_page.py
+++ pages/launched_build_page.py
@@ -9,13 +9,13 @@ class LaunchedBuildPage(BasePage):
         self.launch_id = launch_id
         self.build_id = build_id
         self._endpoint = f'/buildConfiguration/{build_id}/{launch_id}'
-        self.build_status_selector = 'div[role="presentation"] > div:has-text("Success")'
+        self.build_status_selector = 'span[data-test-icon="ok"]'
 
-    #
-    # def go_to_launched_build_page(self):
-    #     with allure.step(f'Go to launched build page with url: {self._get_url(self._endpoint)}'):
-    #         self.actions.go_to_url(self._get_url(self._endpoint))
+    def go_to_launched_build_page(self):
+        with allure.step(f'Go to launched build page with url: {self._get_url(self._endpoint)}'):
+            self.actions.go_to_url(self._get_url(self._endpoint))
 
     def wait_for_success(self):
         with allure.step(f'Wait for selector: {self.build_status_selector} to appear'):
+            self.page.pause()
             self.actions.wait_selector(self.build_status_selector)
