b41b7cd76ee39e1dea29248a08b07e187d560f4e
Apacha1 <mrpepperjam@gmail.com>
2025-05-14 22:07:25 +0200

    change locators

--- api/agents_api.py
+++ api/agents_api.py
@@ -9,6 +9,10 @@ class AgentsApi(CustomRequester):
         response = self.send_request('GET', '/app/rest/agents?locator=connected:true,authorized:false')
         return response
 
+    def get_all_agents(self):
+        response = self.send_request('GET', '/app/rest/agents?locator=authorized:any')
+        return response
+
     def authorize_agent_by_id(self, locator: str):
         self._update_session_headers(**{'Content-Type': 'text/plain', 'Accept': 'text/plain'})
         response = self.send_request('PUT', f'/app/rest/agents/id:{locator}/authorized', json=True)
