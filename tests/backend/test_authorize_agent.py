9d21d6ec33387a75d731df96384b8ee72948397a
Apacha1 <mrpepperjam@gmail.com>
2025-05-14 22:13:07 +0200

    change locators

--- tests/backend/test_authorize_agent.py
+++ tests/backend/test_authorize_agent.py
@@ -5,7 +5,7 @@ class TestAuthorizeAgent:
 
     def test_authorize_agent(self, super_admin):
         agents_list = super_admin.api_manager.agents_api.get_all_agents().json()['agent']
-        assert len(agents_list) > 0, f'List of unauthorized agents is empty: {agents_list}'
+        assert len(agents_list) > 0, f'List of agents is empty: {agents_list}'
         first_agent_id_locator = agents_list[0]['id']
         authorize_response = super_admin.api_manager.agents_api.authorize_agent_by_id(first_agent_id_locator)
         print(authorize_response.text)
